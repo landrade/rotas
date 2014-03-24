4674ba7833fb6e4ab3ebf2dd517e4457c4fc310f
Leonardo Andrade <leonardopandrade@gmail.com>
2014-03-23 23:59:07 -0300

    Validacao de endereco

--- src/main/java/br/com/landrade/routes/domain/Address.java
+++ src/main/java/br/com/landrade/routes/domain/Address.java
@@ -1,17 +1,19 @@
 package br.com.landrade.routes.domain;
 
+import java.util.Objects;
+
 public class Address {
-	
+
 	/* Properties */
-	
+
 	private final String street;
-	
+
 	private final String houseNumber;
-	
+
 	private final String city;
-	
+
 	private final String state;
-	
+
 	/* Constructors */
 
 	/**
@@ -23,12 +25,12 @@ public class Address {
 	 * @param state
 	 */
 	public Address(String street, String houseNumber, String city, String state) {
-		this.street = street;
-		this.houseNumber = houseNumber;
-		this.city = city;
-		this.state = state;
+		this.street = Objects.requireNonNull(street, "Street cannot be null");
+		this.houseNumber = Objects.requireNonNull(houseNumber, "HouseNumber cannot be null");
+		this.city = Objects.requireNonNull(city, "City cannot be null");
+		this.state = Objects.requireNonNull(state, "State cannot be null");
 	}
-	
+
 	/* Getters */
 
 	public String getStreet() {
@@ -46,10 +48,28 @@ public class Address {
 	public String getState() {
 		return state;
 	}
-	
-	
-	
-	
-	
+
+	/* Equals and Hascode */
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(street, houseNumber, city, state);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (obj instanceof Address) {
+			Address other = (Address) obj;
+			return Objects.equals(street, other.street)
+					&& Objects.equals(houseNumber, other.houseNumber)
+					&& Objects.equals(city, other.city)
+					&& Objects.equals(state, other.state);
+		}
+		return false;
+	}
 
 }
